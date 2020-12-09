1687a22173c161a980db604fe52b6f52e99ecaa9
ArlanDaulen <72813488+ArlanDaulen@users.noreply.github.com>
2020-12-09 20:11:27 +0600

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule }   from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
+import { AboutComponent } from './components/about/about.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DataService } from './services/data.service';
+
+const appRoutes: Routes = [
+  { path: '', component: ContentComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    AboutComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/content/content.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '..//..//classes/Movie';
+import { DataService } from '..//..//services/data.service';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  menus = ["/Adventure", "/Action", "/Horror", "/Comedy", "/Detective", "/Cartoon"];
+
+  constructor(private dataService: DataService) { }
+
+  category: Movie[] = [];
+  /*category: any[] = [];*/
+  id: number;
+  name: string;
+  url: string;
+  /*check(index) {
+    switch (index){
+      case 0:
+        this.category = this.dataService.adventuref();
+        break;
+      case 1:
+        this.category = this.dataService.actionf();
+        break;
+      case 2:
+        this.category = this.dataService.horrorf();
+        break;
+      case 3:
+        this.category = this.dataService.comedyf();
+        break;
+      case 4:
+        this.category = this.dataService.detectivef();
+        break;
+      case 5:
+        this.category = this.dataService.cartoonf();
+        break;
+      /*case 6:
+        this.dataService.fantasticf();
+        break;
+      case 7:
+        this.dataService.musicalf();
+        break;
+      case 8:
+        this.dataService.familyf();
+        break;
+      default:
+        break;
+    }
+  }*/
+
+  ngOnInit(): void {
+  this.dataService.getData().subscribe((data: Movie) => this.category = data["Adventure"]);
+  }
+
+}
